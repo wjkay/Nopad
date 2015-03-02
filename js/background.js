30ff1abe0e58b3633206db52b33406cf171dc8d3
Will Kehayioff <will.kehayioff@gmail.com>
2015-03-02 19:36:40 +1300

    Sync save appears to be working correctly. Moved count right

--- js/background.js
+++ js/background.js
@@ -6,58 +6,51 @@ setInterval(function(){operationsInHour = 0;}, 3600000);
 function sync(key, value) {
 	operationsInMinute++;
 	operationsInHour++;
+	console.log('Saving - Key:' + key + ' value:' + value)
 	chrome.storage.sync.set(key, value);
 };
 
 function saveOnClose(note) {
-	// Duplication is evil but note following not working
-	chrome.storage.sync.get('index', function (syncIndex) {
-		syncIndex = syncIndex.index;
-		if (!syncIndex) {syncIndex = {};}
-		if (note.newTitle) {
-			note.title = note.newTitle;
-			note.newTitle = '';
-		}
-		note.date = Date.now();
-		note.new = false;
-		note.changed = false;
-		
-		syncIndex[note.id] = {"id":note.id,"title":note.title,"date":note.date,"active":note.active};
-		
-		chrome.storage.local.set({'index': syncIndex})
-		chrome.storage.sync.set({'index': syncIndex})
-		
-		// Save note
-		var toStore = {};
-		toStore[note.id] = note.body;
-		chrome.storage.local.set(toStore)
-		chrome.storage.sync.set(toStore, function() { })
-	});
+	console.log('Closed - Saving')
+	saveLocalToSync(note);
 }
 
 function saveLocalToSync () {
+	console.log('Beguinning save to sync')
 	chrome.storage.local.get('index', function (local) {
 		var localIndex = local.index;
-		var notes = [];
 		var toSaveCount = 0;
+		var notes = [];
+		
 
-		for (var i = 0;i < localIndex.length; i++) {
-			var note = localIndex[i];
+		for (var id in localIndex) {
+			notes.push(localIndex[id]);
+		}
+		console.log('Got notes: ', notes)
+		for (var i = 0; i < notes.length; i++) { // iteration is issue
+			console.log(i)
 			chrome.storage.local.get(notes[i].id, function (bodyObj) {
+				console.log("Got local note:" + notes[i].id)
+
+				console.log('Got bodyObj: ' + bodyObj[notes[i].id]);
+
 				var body = bodyObj[notes[i].id];
+
 				if (operationsInMinute < 120 && operationsInHour < 1800) { 
-					sync(note.id: body});
+					console.log('Saving: ' + notes[i].title + ' - ' + body);
+					sync(notes[i].id, body);
 					note.changed = false;
 					note.date = new date();
 					toSaveCount--;
 					if (!toSaveCount) {
+						console.log('Saving sync index');
 						sync('index', localIndex)
 					}
 					return;
 				}
 			})
 		}
-	}
+	});
 }
 
 function deleteNotes() {
